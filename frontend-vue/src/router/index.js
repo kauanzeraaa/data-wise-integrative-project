ba5671aedccec932fd44a91a4c631b1a37fa6fab
kauanzeraaa <piacentekauan@gmail.com>
2026-09-20 16:27:07 -0300

    feat: add new routes and delete tailwind.config.js

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from  '../views/Home.vue'
 import Login from  '../views/Login.vue'
+import Contato from  '../views/Contato.vue'
+import Solucoes from  '../views/Solucoes.vue'
+import ComoFunciona from  '../views/ComoFunciona.vue'
 
 const routes = [
     { path : '/', component: Home},
     { path : '/login', component: Login},
+    { path : '/contato', component: Contato},
+    { path : '/solucoes', component: Solucoes},
+    { path : '/como-funciona', component: ComoFunciona}
 ]
 
 const router = createRouter({
